76231edf40d34509e3a89a6cfa6390bcf1d10821
streexs <202380417@psu.palawan.edu.ph>
2024-08-22 22:59:03 +0800

    Jrvcode

--- PS02_02/Program.cs
+++ PS02_02/Program.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PS02_02
+namespace PracticeSets_02
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /*
-            * Write a program that prints all the odd numbers from 1 to 20 using a for loop. 
-            */
+            for (int i = 1; i <= 20; i++)
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            Console.ReadKey();
         }
     }
 }
